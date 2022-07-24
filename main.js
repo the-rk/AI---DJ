313e9c0138d469cee9fcb3c0d766e0a25f2e0531
the-rk <95013057+the-rk@users.noreply.github.com>
2022-07-24 15:35:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+song = "";
+function preload(){
+    song = loadSound("music.mp3");
+}
+function setup(){
+   canvas= createCanvas(500,500);
+   canvas.center();
+
+   video = createCapture(VIDEO);
+   video.hide();
+}
+function draw(){
+image(video, 0,0,500,500);
+}
+function play(){
+    song.play();
+    console.log("it's playing");
+}
