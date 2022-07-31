2a232aa434a4b57b3e0a4f1528285c7a5abcecb0
the-rk <95013057+the-rk@users.noreply.github.com>
2022-07-31 18:21:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftwristx = 0;
+leftwristy = 0;
+rightwristx = 0;
+rightwristy = 0;
 function preload(){
     song = loadSound("music.mp3");
 }
@@ -8,11 +12,30 @@ function setup(){
 
    video = createCapture(VIDEO);
    video.hide();
+   posenet = ml5.poseNet(video, modelloaded);
+   posenet.on("pose", gotposes)
 }
 function draw(){
 image(video, 0,0,500,500);
 }
 function play(){
-    song.play();
-    console.log("it's playing");
+song.play();
+song.setVolume(1);
+song.rate(1);
+}
+function modeloaded(){
+    console.log("posenet is intialised");
+}
+function gotposes(results){
+if (results.length > 0){
+    console.log(results);
+
+    leftwristx = results[0].pose.leftWrist.x;
+    leftwristy = results[0].pose.leftWrist.y;
+    rightwristx = results[0].pose.rightWrist.x;
+    rightwristy = results[0].pose.rightWrist.y;
+console.log("leftwristx = "+ leftwristx + " rightwristx = "+ rightwristx + " leftwristy = "+ leftwristy + " rightwristy = "+ rightwristy);
+
+}
+
 }
